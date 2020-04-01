3e9c1f03d84f38b3a3582d6107f1cca893817cbc
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-04-01 18:52:14 +0800

    Remove built js file

--- mc-ts/CommandHandler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GetDirFiles_1 = require("./utils/GetDirFiles");
-let files = GetDirFiles_1.default('./Commands');
-//# sourceMappingURL=CommandHandler.js.map
