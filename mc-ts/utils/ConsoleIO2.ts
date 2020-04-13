b830c50187ff2cb05cdaf207b4f44f5b7497f0d8
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-04-13 22:48:27 +0800

    Make console support chinese characters input

--- mc-ts/utils/ConsoleIO2.ts
+++ mc-ts/utils/ConsoleIO2.ts
@@ -22,6 +22,7 @@ let Keys = {
     'arrowRight': '\u001B\u005B\u0043',
     'esc': '\u001b'
 }
+let chinese = /[\u3007u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF\u20000-\u2A6DF\u2A700-\u2B73F\u2B740-\u2B81F\u0002F800-\u2FA1F\u2F00-\u2FD5\u2E80-\u2EF3\uFF02\uFF03\uFF04\uFF05\uFF06\uFF07\uFF08\uFF09\uFF0A\uFF0B\uFF0C\uFF0D\uFF0F\uFF1A\uFF1B\uFF1C\uFF1D\uFF1E\uFF20\uFF3B\uFF3C\uFF3D\uFF3E\uFF3F\uFF40\uFF5B\uFF5C\uFF5D\uFF5E\uFF5F\uFF60\uFF62\uFF63\uFF64\u3000\u3001\u3003\u3008\u3009\u300A\u300B\u300C\u300D\u300E\u300F\u3010\u3011\u3014\u3015\u3016\u3017\u3018\u3019\u301A\u301B\u301C\u301D\u301E\u301F\u3030\u303E\u303F\u2013\u2014\u2018\u2019\u201B\u201C\u201D\u201E\u201F\u2026\u2027\uFE4F\uFE51\uFE54\u00B7\uFF01\uFF1F\uFF61\u3002」﹂”』’》）］｝〕〗〙〛〉】]/gm;
 
 stdin.on("data", (key) => {
     switch (key) {
@@ -112,7 +113,7 @@ stdin.on("data", (key) => {
             historyIndex = history.length;
             break;
         default:
-            if (key.charCodeAt() >= 32 && key.charCodeAt() <= 126) {
+            if ((key.charCodeAt() >= 32 && key.charCodeAt() <= 126) || (key.match(chinese))) {
                 for (let i = 0; i < key.length; i++) {
                     // normal char, write to buffer
                     buffer += key[i];
